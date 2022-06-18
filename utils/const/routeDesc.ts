9ce884355878617ab3373cded3c11aa9e1618541
jkh2801 <jkh2801@naver.com>
2022-06-18 10:46:38 +0900

    feat: updating Homepage for routeDesc

--- utils/const/routeDesc.ts
+++ utils/const/routeDesc.ts
@@ -1,6 +1,7 @@
 import balancing from '@assets/thumbnail_balancing.png';
 import markdown from '@assets/thumbnail_markdown.png';
 import receipt from '@assets/thumbnail_receipt.png';
+import draggable from '@assets/thumbnail_draggable.png';
 import { RouteDescType } from '@customTypes/HomeTypes';
 
 export const routeDesc: RouteDescType = {
@@ -9,6 +10,12 @@ export const routeDesc: RouteDescType = {
     desc: `Balancing Page
     This is a simple design page. I just wanted to express the scale of both arms according to the weight. 
     Please enter a number on the scale. You can enter only natural numbers on a scale. You'll be able to see the dynamic changes with the number of inputs.`,
+  },
+  draggable: {
+    thumbnail: draggable,
+    desc: `Draggable Page
+    This is a simple draggable page. It is designed for the purpose of testing the react-draggable library.
+    Please drag the contents in the box. You will be able to check the smooth drag movement.`,
   },
 
   markdown: {
